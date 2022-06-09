997999edf8be853cd559ae76b9c045bac362f04c
mathew <mathew@mathews-MacBook-Pro.local>
2022-06-08 23:07:26 -0400

    returns byte size and bit size

--- src/importFile.java
+++ src/importFile.java
@@ -13,22 +13,40 @@ public class importFile {
 
     public static void selectFile(String arg){
 
-        String fileName = "/Users/mathew/repos/slow_down/Take_it_slow/lib/media/" + arg;
+        String fileName = "/Users/mathew/repos/take_it_slow/lib/media/" + arg;
         File   input = new File(fileName);
-
-        try{
+	
+       	try {
+            // establishes input file as audio stream
             AudioInputStream audiofile = AudioSystem.getAudioInputStream(input);
-            audiofile.get
+
+            // number of bytes
+            int frameLength = (int) audiofile.getFrameLength();
+
+            // number of frames
+            int numFrames = (int) audiofile.getFormat().getFrameSize();
+
+            // creates byte array
+            byte[] bytes = new byte[frameLength * numFrames];
+
+            // reads data to array
+            int songData = 0;
+            songData = audiofile.read(bytes);
+
+            
+            int bitSize = audiofile.getFormat().getSampleSizeInBits();
+            
+            
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
+            
 
-        
     }
 
 
     public static void main(String[] args) {
         selectFile("sample_wav.wav");
-
+        
     }
 }
