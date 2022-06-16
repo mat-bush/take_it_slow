c83a9582343fc34e20ebfa698467c7ee61001164
mathew <mathew@mathews-MacBook-Pro.local>
2022-06-15 22:49:05 -0400

    restructured class

--- src/streamData.java
+++ src/streamData.java
@@ -1,28 +1,102 @@
+import javax.sound.sampled.AudioInputStream;
+
 public class streamData {
     
+    // inits used attributes
+    private AudioInputStream audioFile; // audio stream
+    private int frameLength;
+    private int numFrames;
+    private byte[] bytes;
+    private int songData;
+    private int bitSize;
+
+    public void setAudioFile(AudioInputStream arg){
+        this.audioFile = arg;
+    }
+
+    public AudioInputStream getAudioFile(){
+        return audioFile;
+    }
+
+
+    public void setFrameLength(){
+        try {
+            this.frameLength = (int) audioFile.getFrameLength();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getFrameLength(){
+        return frameLength;
+    }
+
+    public void setNumFrames(){
+        try {
+            this.numFrames = (int) audioFile.getFormat().getFrameSize();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getNumFrames(){
+        return numFrames;
+    }
+
+    public void setBytes(){
+        try {
+            this.bytes = new byte[this.frameLength * this.numFrames];
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public byte[] getBytes(){
+        return bytes;
+    }
+
+    public void setSongData(){
+        try {
+            this.songData = audioFile.read(this.bytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getSongData(){
+        return songData;
+    }
+
+    public void setBitSize(){
+        try {
+            this.bitSize = audioFile.getFormat().getSampleSizeInBits();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getBitSize(){
+        return bitSize;
+    }
+
+    public void writeSamples(){
+        
+    }
+
+    public static void main(AudioInputStream arg) {
+        streamData sd = new streamData();
+
+        // sets attributes 
+        sd.setAudioFile(arg);
+        sd.setFrameLength();
+        sd.setNumFrames();
+        sd.setBytes();
+        sd.setSongData();
+        sd.setBitSize();
+    }
 }
 
-    // try {
-    //     // establishes input file as audio stream
-    //     AudioInputStream audiofile = AudioSystem.getAudioInputStream(input);
+    
 
-    //     // // number of bytes
-    //     // int frameLength = (int) audiofile.getFrameLength();
 
-    //     // // number of frames
-    //     // int numFrames = (int) audiofile.getFormat().getFrameSize();
-
-    //     // // creates byte array
-    //     // byte[] bytes = new byte[frameLength * numFrames];
-
-    //     // // reads data to array
-    //     // int songData = 0;
-    //     // songData = audiofile.read(bytes);
-
-        
-    //     // int bitSize = audiofile.getFormat().getSampleSizeInBits();
-        
-        
-    // } catch (Exception e) {
-    //     e.printStackTrace();
-    // }
+   
