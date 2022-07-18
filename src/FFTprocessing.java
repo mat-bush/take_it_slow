2882e2564c26cb23c6792d5ed767e097da85aa0b
mathew <mathew@mathews-MacBook-Pro.local>
2022-07-17 20:47:54 -0400

    creating the class

---
+++ src/FFTprocessing.java
@@ -0,0 +1,22 @@
+// What this is:
+//  This is the class that processes the data coming from the out come of FFT
+
+
+
+// plans for this aspect of the project:
+//  How the process works:
+//  1) data sent in the form of Array of Floats [t,[array of frequencies], [array of amplitudes]
+//                                               [t + ∆ ] , [array of frequencies], [array of amplitudes]]
+//  2) iterate through the array from step 1 and:
+//      a) round frequencies in array
+//  3) create new array w/ time, Amplitude, and radian values
+//      a) write to txt doc(s)
+//  4) grep for each unique time and pull lines w/ each rad value and amplitude at a given and sum
+// 
+
+
+
+
+public class FFTprocessing {
+    
+}
