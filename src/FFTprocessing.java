336440e0ee84b47659e89812e8254014bc8d053b
mathew <mathew@mathews-MacBook-Pro.local>
2022-07-17 22:41:34 -0400

    accomplished steps: 1a,2a,and 2bi

--- src/FFTprocessing.java
+++ src/FFTprocessing.java
@@ -7,12 +7,15 @@
 //  How the process works:
 //  1) data sent in the form of Array of Floats [t,[array of frequencies], [array of amplitudes]
 //                                               [t + ∆ ] , [array of frequencies], [array of amplitudes]]
-//  2) iterate through the array from step 1 and:
-//      a) round frequencies in array
+//      a) create object for input w/ get and set methods: accomplished
+//  2) iterate through the array from step 1 and
+//      a) round frequencies in array: accomplished
 //         i) create method to round to desired accuracy: accomplished
-//  3) create new array w/ time, Amplitude, and radian values
-//  4) write to txt doc(s)
-//  5) grep for each unique time and pull lines w/ each rad value and amplitude at a given and sum
+//        ii) create new array for rounded frequcines and times w/ get and set: accomplished
+//      b) create new array w/ time, Amplitude, and radian values
+//         i) create object for new array w/ get and set methods: accomplished
+//  3) write to txt doc(s)
+//  4) grep for each unique time and pull lines w/ each rad value and amplitude at a given and sum
 // 
 
 
@@ -20,8 +23,27 @@
 
 public class FFTprocessing {
 // object that holds array from step one
-    private float[][][][] FFTdata;
+    private float[][][] RawFFTdata; // 1a
+    private float[][][] RoundFFTdata; // 2aii
+    private float[][][] TAR; // (Time, Amplitude, Radian) 2bi
 
+    // for step 1a
+    public void setRawFFT(float[][][] arg){
+        this.RawFFTdata = arg;
+    }
+
+    public float[][][] getRawFFT(){
+        return this.RawFFTdata;
+    }
+
+    // for step 2aii
+    public void setRoundFFT(float[][][] arg){
+        this.RoundFFTdata = arg;
+    }
+
+    public float[][][] getRoundFFT(){
+        return this.RoundFFTdata;
+    }
 
     // rounding method for 2ai 
     public float[] roundToFloats(float[] floats, Double decPlaces){
@@ -46,6 +68,24 @@ public class FFTprocessing {
 
         return roundedFloats;
     }
+
+    // does bulk of processing for FFT data (steps 2a, 2b, and 3)
+    public void RoundsArray(){
+        // iterates through RawFFT
+        for (int i = 0; i < this.RawFFTdata[0].length; i++){
+            this.RawFFTdata[1][i] = roundToFloats(this.RawFFTdata[1][i], 0.0);
+        }
+    }
+
+    // for step 2aii
+    public void setTAR(float[][][] arg){
+        this.TAR = arg;
+    }
+
+    public float[][][] getTAR(){
+        return this.TAR;
+    }
+
 
 
 }
