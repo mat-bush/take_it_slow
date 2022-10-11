750ed0c8ca3b02f7b397f886456dc519a828fdfe
mathew <mathew@mathews-MacBook-Pro.local>
2022-10-11 14:54:21 -0400

    base gui

---
+++ src/gui.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class gui {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame(); // inits frame
+
+        // sets frame properties
+        frame.setTitle("Take It Slow");
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        
+    }
+
+
+}
